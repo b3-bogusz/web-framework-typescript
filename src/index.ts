d374dcab5112e4ffc4cc407d7f6e180ed13ff0c4
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-19 12:12:15 +0200

    create User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello world')
+import {User} from "../models/User";
+
+const user1 = new User({ name: 'Patryk', age: 31 });
+
+user1.set({ name: 'newName', age: 20 })
+
+console.log(user1.get('age'));
