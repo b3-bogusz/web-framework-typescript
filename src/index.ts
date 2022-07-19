17edeca3e0ebd8e7881027cea768994fdecf15fe
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-19 13:34:39 +0200

    add eventing system

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {User} from "../models/User";
 
 const user1 = new User({ name: 'Patryk', age: 31 });
 
-user1.set({ name: 'newName', age: 20 })
-
-console.log(user1.get('age'));
+user1.on('change', () => {
+    console.log('Change #1')
+});
+user1.trigger('change')
