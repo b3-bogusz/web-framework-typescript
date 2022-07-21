714857ea1c4fbe75752f281d733045dde63b93ee
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-21 10:15:08 +0200

    Refactor functionality using composition style approach

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import {User} from "../models/User";
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.save()
+user.on('change', () => {
+    console.log('user was changed')
+})
+
+user.set({ name: 'New name' });
