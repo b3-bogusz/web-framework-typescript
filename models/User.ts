24212cf3a923316179e5c70c4448bb8b03b29b1c
Patryk Bogusz <b3.bogusz@gmail.com>
2022-07-21 12:31:05 +0200

    add User Collection class

--- models/User.ts
+++ models/User.ts
@@ -2,6 +2,7 @@ import {Model} from "./Model";
 import {Attributes} from "./Attributes";
 import {Eventing} from "./Eventings";
 import {ApiSync} from "./ApiSync";
+import {Collection} from "./Collection";
 
 export interface UserProps {
     name?: string;
@@ -12,11 +13,19 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps>{
+
     static buildUser(attrs: UserProps): User {
         return new User(
             new Attributes<UserProps>(attrs),
             new Eventing(),
             new ApiSync<UserProps>(rootUrl)
+        )
+    }
+
+    static buildUserCollection(): Collection<User, UserProps> {
+        return new Collection<User, UserProps>(
+            rootUrl,
+            (json: UserProps) => User.buildUser(json)
         )
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import {User} from "../models/User";
 
-const user = User.buildUser({ name: 'new record', age: 0, id: 10 });
-
-user.on('change', () => {
-    console.log(user)
+const collection = User.buildUserCollection();
+collection.on('change', () => {
+    console.log(collection)
 })
 
-user.fetch();
+collection.fetch();
